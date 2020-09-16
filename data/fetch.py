c71c2f65231d925693702d1d5f32aea0bcf5f796
diskontinuum <27351364+diskontinuum@users.noreply.github.com>
2020-09-16 11:52:04 +0200

    Completed script that downloads and saves data. Started main script.

--- data/fetch.py
+++ data/fetch.py
@@ -1,7 +1,105 @@
-import pandas as pd
+import json
+import os
+import requests
 
-# get data
-url_problems = "https://github.com/e-sr/moonboard/blob/master/problems/fetch/moonboard_problems_setup_2016.json"
-url_setup = "https://github.com/e-sr/moonboard/blob/master/problems/HoldSetup.json"
-problems = pd.read_json(url_problems)
-setup = pd.read_json(url_setup)
+"""
+Gets and saves the .json files from github that specify the 2016 moonboard setup
+and the collection of problems. 
+"""
+############################### Hard-coded #####################################
+PATHS = {"input":{}, "output":{}}
+PATHS["input"]["setup"]= "https://raw.github.com/e-sr/moonboard/master/problems/HoldSetup.json"
+PATHS["input"]["problems"]= "https://raw.github.com/e-sr/moonboard/master/problems/fetch/moonboard_problems_setup_2016.json"
+PATHS["output"]["setup"] = os.path.join(os.getcwd(), "setup.json")
+PATHS["output"]["problems"] = os.path.join(os.getcwd(), "problems_2016.json")
+################################################################################
+
+class BoardData():
+    def __init__(self, input, output):
+        self.url = input
+        self.outpath = output
+    def fetch(self):
+        data = requests.get(self.url)
+        self.data = data.json()
+        print(self.data)
+    def write(self):
+        with open(self.outpath, 'w') as outfile:
+            json.dump(self.data, outfile)
+
+# get setup data
+setup = BoardData(input=PATHS["input"]["setup"],output=PATHS["output"]["setup"])
+setup.fetch()
+setup.write()
+# get problems data
+problems = BoardData(input=PATHS["input"]["problems"],output=PATHS["output"]["problems"])
+problems.fetch()
+problems.write()
+
+"""
+**setup structure**
+ "2016": {
+      "H7": {
+         "HoldSet": "OS",
+         "Hold": 1,
+         "Orientation": "SE"
+**problems structure** "341208": {
+      "Method": "Feet follow hands",
+      "Name": "OFF THE MATTRESS",
+      "Grade": "8A+",
+      "MoonBoardConfigurationId": 0,
+      "Setter": {
+         "Nickname": "bcy",
+         "Firstname": "Bocheng",
+         "Lastname": "Yang"
+      },
+      "Rating": 0,
+      "Repeats": 0,
+      "Holdsetup": {
+         "Id": 1,
+         "Description": "MoonBoard 2016",
+         "HoldLayoutId": 0
+      },
+      "IsBenchmark": false,
+      "IsAssessmentProblem": false,
+      "Moves": [
+         {
+            "Id": 1910055,
+            "Description": "K18",
+            "IsStart": false,
+            "IsEnd": true
+         },
+         {
+            "Id": 1910056,
+            "Description": "K16",
+            "IsStart": false,
+            "IsEnd": false
+         },
+         {
+            "Id": 1910057,
+            "Description": "F15",
+            "IsStart": false,
+            "IsEnd": false
+         },
+         {
+            "Id": 1910058,
+            "Description": "B11",
+            "IsStart": false,
+            "IsEnd": false
+         },
+         {
+            "Id": 1910059,
+            "Description": "A5",
+            "IsStart": true,
+            "IsEnd": false
+         },
+         {
+            "Id": 1910060,
+            "Description": "H15",
+            "IsStart": false,
+            "IsEnd": false
+         }
+      ],
+      "DateInserted": "/Date(1554298797933)/"
+   },
+   "341206":
+"""
---
+++ main.py
@@ -0,0 +1,17 @@
+"""
+The moonboard is a fixed-size grid
+ indexed by capitalized roman letters {A,...,K} horizontally and
+ indexed by integers {1,...,18} vertically.
+ Each grid point has a fixed hold attached to it.
+ A given setup specifiex the hold type and hold orientation
+ on every grid point.
+ For every official setup there is a collection of boulder problems, which
+ are specified simply as set of coordinates, designating the holds
+  that may be used when climbing the board.
+
+ Here we consider the 2016 moonboard setup, which has the largest
+ collection of problems (TODO: verify).
+ """
+ import data
+
+ #import data
